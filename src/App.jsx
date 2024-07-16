94efc01430100b687abb19ddf789af29d7b7887b
elijah571 <elijahfx43@gmail.com>
2024-07-16 21:14:53 +0100

    my works

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import About from "./Components/About/About";
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 import Services from "./Components/Services/Services";
+import MyWork from "./Components/MyWork/MyWork";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
      <Hero/>
      <About/>
      <Services/> 
+    <MyWork/>
     </div>
   )
 }
