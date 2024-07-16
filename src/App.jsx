0674ec3059fef90db9fd6ec47b153e009ac87f70
elijah571 <elijahfx43@gmail.com>
2024-07-17 00:45:11 +0100

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 import Services from "./Components/Services/Services";
 import MyWork from "./Components/MyWork/MyWork";
+import Contact from "./Components/Contact/Contact";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
      <About/>
      <Services/> 
     <MyWork/>
+    <Contact/>
     </div>
   )
 }
