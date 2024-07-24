ffa8218801985700a976d4e2fe380659072c7c4e
elijah571 <elijahfx43@gmail.com>
2024-07-24 20:42:28 +0100

    projects added

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -1,4 +1,4 @@
-import './MyWork.css'
+import './MyWork.css';
 import theme_pattern from '../../assets/theme_pattern.svg';
 import mywork_data from '../../assets/mywork_data';
 import arrow_icon from '../../assets/arrow_icon.svg';
@@ -6,21 +6,23 @@ import arrow_icon from '../../assets/arrow_icon.svg';
 function MyWork() {
   return (
     <div id='work' className='mywork'>
-        <div className="mywork-title">
-            <h1>My latest work</h1>
-            <img src={theme_pattern} alt="" />
-        </div>
-        <div className="mywork-container">
-            {mywork_data.map((work, index) => {
-              return <img key={index} src={work.w_img}/>
-            })}
-        </div> 
-        <div className="mywork-showmore">
-          <p>Show More</p>
-          <img src={arrow_icon} alt="" />
-        </div>
+      <div className="mywork-title">
+        <h1>My latest work</h1>
+        <img src={theme_pattern} alt="Theme pattern" />
+      </div>
+      <div className="mywork-container">
+        {mywork_data.map((work, index) => (
+          <a key={index} href={work.url} target="_blank" rel="noopener noreferrer">
+            <img src={work.w_img} alt={work.w_name} />
+          </a>
+        ))}
+      </div>
+      <div className="mywork-showmore">
+        <p>Show More</p>
+        <img src={arrow_icon} alt="Arrow icon" />
+      </div>
     </div>
-  ) 
+  );
 }
 
 export default MyWork;
--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,41 +1,47 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+import project_img1 from '../assets/project_1.PNG';
+import project_img2 from '../assets/project_2.PNG';
+import project_img3 from '../assets/project_3.PNG';
+import project_img4 from '../assets/project_4.PNG';
+import project_img5 from '../assets/project_5.PNG';
+import project_img6 from '../assets/project_6.PNG';
 
 const mywork_data = [
     {
-        w_no:1,
-        w_name:"Web design",
-        w_img:project1_img
+        w_no: 1,
+        w_name: "Web design",
+        w_img: project_img1,
+        url: "https://elijah571.github.io/dash_board/"  
     },
     {
-        w_no:2,
-        w_name:"Web design",
-        w_img:project2_img
+        w_no: 2,
+        w_name: "Web design",
+        w_img: project_img2,
+        url: "https://elijah571.github.io/credit-cards/"  
     },
     {
-        w_no:3,
-        w_name:"Web design",
-        w_img:project3_img
+        w_no: 3,
+        w_name: "Web design",
+        w_img: project_img3,
+        url: "https://elijah571.github.io/resume/" 
     },
     {
-        w_no:4,
-        w_name:"Web design",
-        w_img:project4_img
+        w_no: 4,
+        w_name: "Web design",
+        w_img: project_img4,
+        url: "https://elijah571.github.io/form/"  
     },
     {
-        w_no:5,
-        w_name:"Web design",
-        w_img:project5_img
+        w_no: 5,
+        w_name: "Web design",
+        w_img: project_img5,
+        url: "https://elijah571.github.io/rock-paper-scissor_game/" 
     },
     {
-        w_no:6,
-        w_name:"Web design",
-        w_img:project6_img
+        w_no: 6,
+        w_name: "Web design",
+        w_img: project_img6,
+        url: "https://elijah571.github.io/calculator/"  
     },
 ]
- 
+
 export default mywork_data;
