ad3451fd80a6af62c91502f06276f2f064f41da8
elijah571 <elijahfx43@gmail.com>
2024-07-25 20:16:28 +0100

    changes made

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import './Footer.css';
 import logo from '../../assets/logo.svg';
-import user_icon from '../../assets/user_icon.svg'; 
+// import user_icon from '../../assets/user_icon.svg'; 
 
 const Footer = () => {
     return(
@@ -10,13 +10,13 @@ const Footer = () => {
                     <img className='logo' src={logo} alt="" />
                     <p>I'm a frontend developer. Passionate frontend developer with a knack for crafting responsive, user-friendly web interfaces using modern technologies like React, HTML, CSS,</p>
                 </div>
-                <div className="footer-top-right">
+                {/* <div className="footer-top-right">
                     <div className='footer-email-input'>
                         <img src={user_icon} alt="" />
                         <input type="email" placeholder='Enter your email' />
                     </div>
                     <div className="footer-subscribe">Subscribe</div>
-                </div>
+                </div> */}
             </div>
             <hr />
             <div className="footer-bottom">
