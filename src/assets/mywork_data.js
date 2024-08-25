4873977939dbb540a2d3db624037fbffffe055bf
elijah571 <elijahfx43@gmail.com>
2024-08-25 19:07:12 +0100

    file remove

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,4 +1,4 @@
-import project_img1 from '../assets/project_1.png';
+// import project_img1 from '../assets/project_1.png';
 import project_img2 from '../assets/project_2.png';
 import project_img3 from '../assets/project_3.PNG';
 import project_img4 from '../assets/project_4.PNG';
@@ -6,12 +6,12 @@ import project_img5 from '../assets/project_5.PNG';
 import project_img6 from '../assets/project_6.PNG';
 
 const mywork_data = [
-    {
-        w_no: 1,
-        w_name: "Web design",
-        w_img: project_img1,
-        url: "https://food-delivery-jxgs.vercel.app/"  
-    },
+    // {
+    //     w_no: 1,
+    //     w_name: "Web design",
+    //     w_img: project_img1,
+    //     url: "https://food-delivery-jxgs.vercel.app/"  
+    // },
     {
         w_no: 2,
         w_name: "Web design",
